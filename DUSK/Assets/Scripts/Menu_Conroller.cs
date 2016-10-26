ec912bbe925b96e77aec0011d63c08b540683b7e
clothoff <clothoff@gmail.com>
2016-10-26 02:52:57 -0400

    tested blended lamp model and added texture,Added a new startmenu scene for future use

---
+++ DUSK/Assets/Scripts/Menu_Conroller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu_Conroller : MonoBehaviour {
+
+	public Canvas Startmenu;
+	public Button Startgame_button;
+	public Button Instruction_button;
+	public Button Options_button;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void Enter_LevelChoosing_Menu () {
+		
+	}
+
+	public void Enter_Instruction_Menu () {
+
+	}
+
+	public void Enter_Options_Menu () {
+
+	}
+
+
+}
