429c2a7dd9bb25b120b869738508436543da5eda
Kha Pham <phamkha@Khas-MacBook-Pro.local>
2016-11-05 04:02:22 -0400

    Add tutorial design

---
+++ DUSK/Assets/Scripts/Trigger_Controller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger_Controller : MonoBehaviour {
+
+	public Light controlled_light;
+
+	private bool is_triggered;
+
+	// Use this for initialization
+	void Start () {
+		is_triggered = false;
+	}
+
+	public void Triggered(){
+		if (is_triggered == false) {
+			gameObject.GetComponent<Renderer> ().material.color = Color.green;
+			if (controlled_light.intensity > 0) {
+				controlled_light.gameObject.SetActive(false);
+			}
+		}
+		is_triggered = true;
+	}
+
+
+
+}
